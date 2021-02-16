c6563d216cef12d0ca4f22a966ee37297c1d3b8d
Brandon Hung <brandonhung343@gmail.com>
2021-02-15 17:50:14 -0800

    Test 1

--- led_paintbrush/led_paintbrush.ino
+++ led_paintbrush/led_paintbrush.ino
@@ -1,12 +1,28 @@
 #include <Adafruit_DotStar.h>
 #include <Adafruit_BNO055.h>
+#include <SPI.h>
+#define NUMPX 144
+#define MAXBRIGHT 255
+Adafruit_DotStar strip(NUMPX, DOTSTAR_BRG);
+int set_brightness_percentage(float percent){
+  return int(percent * MAXBRIGHT);
+}
 
 void setup() {
   // put your setup code here, to run once:
-
+ 
+  strip.begin();
+  strip.show();
+  strip.setBrightness(set_brightness_percentage(0.1));
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  for(int i = 0; i < NUMPX; i++)
+  {
+    strip.setPixelColor(i, 255, 255, 255);
+    delay(10);
+    strip.clear();
+  }
 
 }
